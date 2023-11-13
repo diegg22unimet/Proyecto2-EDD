80dfcc1b7dce0f5087572f8ca92d1073c190a540
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2023-11-12 23:50:17 -0400

    Creación clases list y node

---
+++ src/coladeimpresion/List.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coladeimpresion;
+
+/**
+ *
+ * @author manza
+ */
+public class List<T> {
+    private Node<T> head;
+    private int size;
+
+    public List() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public void setHead(Node<T> head) {
+        this.head = head;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    public boolean isEmpty() {
+        return getHead() == null;
+    }
+    
+    public void append(T data) {
+        Node<T> node = new Node<>(data);
+        if (isEmpty()) {
+            setHead(node);
+        } else {
+            Node<T> pointer = getHead();
+            while (pointer.getNext() != null) {                
+                pointer = pointer.getNext();
+            }
+            pointer.setNext(node);
+        }
+    }
+    
+    public T get(int index) {
+        if (isEmpty()) {
+            return null;
+        } else {
+            if (index == 0) {
+                return getHead().getData();
+            } else {
+                Node<T> pointer = getHead();
+                int i = 1;
+                while (pointer.getNext() != null) {                    
+                    if (i == index) {
+                        return pointer.getNext().getData();
+                    } else {
+                        pointer = pointer.getNext();
+                        i++;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+    
+}
---
+++ src/coladeimpresion/Node.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coladeimpresion;
+
+/**
+ *
+ * @author manza
+ */
+public class Node<T> {
+    private T data;
+    private Node<T> next;
+
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+    
+}
