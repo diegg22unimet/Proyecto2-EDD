da57e03d34d37308b46e4514cd65b06f1485af93
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2023-11-12 23:25:56 -0400

    Añadidas clases user y document

---
+++ src/coladeimpresion/Document.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coladeimpresion;
+
+/**
+ *
+ * @author manza
+ */
+public class Document {
+    private String name;
+    private String type;
+    private int size;
+
+    public Document(String name, String type, int size) {
+        this.name = name;
+        this.type = type;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    
+    
+}
---
+++ src/coladeimpresion/User.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coladeimpresion;
+
+/**
+ *
+ * @author manza
+ */
+public class User {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
