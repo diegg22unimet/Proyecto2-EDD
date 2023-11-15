b60409818b192253e642164b8aa1ba60020caa0c
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2023-11-15 17:47:08 -0400

    Añadida interfaz de usuario

--- src/coladeimpresion/Main.java
+++ src/coladeimpresion/Main.java
@@ -14,7 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Interfaz fw = new Interfaz();
+        fw.setVisible(true);
     }
     
 }
