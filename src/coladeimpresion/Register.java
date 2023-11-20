cdec25f4ba361f010747f09cfc97b57d977d8abb
diegg22unimet <dmanzanilla@correo.unimet.edu.ve>
2023-11-20 18:23:28 -0400

    Añadida clase register

---
+++ src/coladeimpresion/Register.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package coladeimpresion;
+
+/**
+ *
+ * @author manza
+ */
+public class Register {
+    private Document document;
+    private int time;
+
+    public Register(Document document, int time) {
+        this.document = document;
+        this.time = time;
+    }
+
+    public Document getDocument() {
+        return document;
+    }
+
+    public void setDocument(Document document) {
+        this.document = document;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+}
